c0059ef92c0f94bbfeed1b0a08dc2a2da82d94b0
kamicreator <kamicreator@gmail.com>
2023-01-07 18:22:26 +0800

    [add] Build spring boot project & readme

---
+++ src/test/java/com/zens/SpringBootPPTXToPDF/SpringBootPptxToPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zens.SpringBootPPTXToPDF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPptxToPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
